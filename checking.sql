bda7d7bc2690495bf6e92d35f4186ce7db5067d0
Kurapati Toyesh <ktoyesh04@gmail.com>
2023-05-08 21:09:17 +0530

    4th check

--- checking.sql
+++ checking.sql
@@ -2,4 +2,11 @@ use first;
 create table First(name varchar(5));
 insert into first values('Rama');
 insert into first values('Krsna');
+insert into first values('Shiva');
+insert into first values('Hanuma');
 select * from first;
+
+create table Second(num int);
+insert into second values(1);
+insert into second values(2);
+insert into second values(3);
