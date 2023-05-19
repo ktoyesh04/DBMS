28d76f41c3e10488c024f822b59823a6d9240221
Kurapati Toyesh <124575344+ktoyesh04@users.noreply.github.com>
2023-05-19 18:03:23 +0530

    added pk, fk constraints to entity schemas.

--- entity_schemas.sql
+++ entity_schemas.sql
@@ -1,33 +1,38 @@
+CREATE TABLE Admin(
+    aid varchar(10) PRIMARY KEY,
+    aname varchar(20) NOT NULL,
+    loginname varchar(10) UNIQUE,
+    password varchar(8) NOT NULL
+);
+
 CREATE TABLE Student(
-	sid varchar(10) PRIMARY KEY,
+    sid varchar(10),
+    aid varchar(10) NOT NULL,
     sname varchar(20) NOT NULL,
     gender varchar(1) NOT NULL,
     mail varchar(30) UNIQUE,
     phone int UNIQUE,
     dob date NOT NULL,
-    address varchar(50) NOT NULL,
-    gpa real,
-    attendence real
-);
-
-CREATE TABLE Admin(
-    a_id varchar(10) PRIMARY KEY,
-	a_name varchar(20) NOT NULL,
-    login_name varchar(10) UNIQUE,
-    password varchar(8) NOT NULL
+    address varchar(30) NOT NULL, 
+    CONSTRAINT s_pk PRIMARY KEY(sid),
+    CONSTRAINT s_fk FOREIGN KEY(aid) REFERENCES Admin(aid)
 );
 
 CREATE TABLE Course(
-	cid varchar(5) ,
+    cid varchar(5) ,
+    aid varchar(10) NOT NULL,
     cname varchar(20) NOT NULL,
     cfee int NOT NULL,
     duration_in_weeks int NOT NULL,
-    CONSTRAINT c_pk PRIMARY KEY(cid)
+    CONSTRAINT c_pk PRIMARY KEY(cid),
+    CONSTRAINT c_fk FOREIGN KEY(aid) REFERENCES Admin(aid)
 );
 
 CREATE TABLE Faculty(
-	fid varchar(10),
+    fid varchar(10),
+    aid varchar(10) NOT NULL,
     fname varchar(20) NOT NULL,
     position varchar(20) NOT NULL,
-    CONSTRAINT f_pk PRIMARY KEY(fid)
+    CONSTRAINT f_pk PRIMARY KEY(fid),
+    CONSTRAINT f_fk FOREIGN KEY(aid) REFERENCES Admin(aid)
 );
